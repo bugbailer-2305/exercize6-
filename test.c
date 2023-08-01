cc5905c4fb459eec5ea0414f793e4ce8586072e1
Barry <barryli677@gmail.com>
2023-07-31 20:17:59 -0400

    union sum and max

--- test.c
+++ test.c
@@ -3,39 +3,24 @@
 #include <stdlib.h>
 #include <string.h>
 
-struct ThreadArgs
-{
+struct ThreadArgs {
   int *array_pos;
   int length;
 };
 
 void generate_r_array(int *r_array, int num_elements, int seed);
+void *get_sum(int *array_pos, int length);
+void *max(int *array, int length);
 
-int main()
-{
-  int *r_array;
-  int num_elements;
-  int threads;
+int main(int argc, char *argv[]) {
+  int task = atoi(argv[0]);
+  int seed = atoi(argv[1]);
+  int num_elements = atoi(argv[2]);
+  int threads = atoi(argv[3]);
+  char print_results = atoi(argv[4]);
   pthread_t *thread_ids;
-  int seed;
-  int task;
-  char print_results;
+  int *r_array;
   int i = 0;
-
-  printf("Enter Tasks(1 for max, 2 for sum): ");
-  scanf("%d", &task);
-
-  printf("Enter Seed: ");
-  scanf("%d", &seed);
-
-  printf("Enter Number of elements: ");
-  scanf("%d", &num_elements);
-
-  printf("Enter Number of Threads: ");
-  scanf("%d", &threads);
-
-  printf("Print Results?(Y for yes)");
-  scanf(" %c", &print_results);
 
   printf("You've entered: \nTask: %d \nSeed: %d \nElement Number: %d \nThread "
          "Number: %d \nPrint "
@@ -45,37 +30,44 @@ int main()
   r_array = malloc(num_elements * sizeof(int));
   generate_r_array(r_array, num_elements, seed);
 
-  for (i = 0; i < num_elements; i++)
-  {
+  for (i = 0; i < num_elements; i++) {
     printf("%d\n", r_array[i]);
   }
   free(r_array);
   return 0;
 }
 
-void generate_r_array(int *r_array, int num_elements, int seed)
-{
+void generate_r_array(int *r_array, int num_elements, int seed) {
   int i;
 
   srand(seed);
 
-  for (i = 0; i < num_elements; i++)
-  {
+  for (i = 0; i < num_elements; i++) {
     r_array[i] = rand();
   }
 }
 
-void *max(int *array, int length)
-{
+void *get_sum(int *array_pos, int length) {
+  int i;
+  int *sum = malloc(sizeof(int));
+
+  *sum = 0;
+
+  for (i = 0; i < length; i++) {
+    *sum = (*sum + array_pos[i]) % 1000000;
+  }
+
+  return sum;
+}
+
+void *max(int *array, int length) {
   int *max = malloc(sizeof(int));
   int i;
   *max = array[0];
 
   /* Iterate through the array and update max if a larger element is found*/
-  for (i = 1; i < length; i++)
-  {
-    if (array[i] > *max)
-    {
+  for (i = 1; i < length; i++) {
+    if (array[i] > *max) {
       *max = array[i];
     }
   }
