cb90c6ab734b1ae9440d24a146f06b54b9f80b9d
Naitik Gupta <naitikguptarocks@gmail.com>
2023-07-31 20:50:15 -0400

    adding thread basics

--- test.c
+++ test.c
@@ -3,7 +3,8 @@
 #include <stdlib.h>
 #include <string.h>
 
-struct ThreadArgs {
+struct ThreadArgs
+{
   int *array_pos;
   int length;
 };
@@ -12,7 +13,31 @@ void generate_r_array(int *r_array, int num_elements, int seed);
 void *get_sum(int *array_pos, int length);
 void *max(int *array, int length);
 
-int main(int argc, char *argv[]) {
+void threads_function(function_type func, int max_threads)
+{
+  pthread_t tids[max_threads + 1];
+  int i, ids[max_threads + 1];
+
+  /* Creating threads */
+  for (i = 1; i <= max_threads; i++)
+  {
+    ids[i] = i;
+    pthread_create(&tids[i], NULL, func, &ids[i]);
+    printf("Thread 0 created thread %d\n", i);
+  }
+
+  /* Waiting for threads to finish */
+  for (i = 1; i <= THREAD_CT; i++)
+  {
+    pthread_join(tids[i], NULL);
+    printf("Thread 0 reaped thread %d\n", i);
+  }
+
+  return 0;
+}
+
+int main(int argc, char *argv[])
+{
   int task = atoi(argv[0]);
   int seed = atoi(argv[1]);
   int num_elements = atoi(argv[2]);
@@ -30,44 +55,52 @@ int main(int argc, char *argv[]) {
   r_array = malloc(num_elements * sizeof(int));
   generate_r_array(r_array, num_elements, seed);
 
-  for (i = 0; i < num_elements; i++) {
+  for (i = 0; i < num_elements; i++)
+  {
     printf("%d\n", r_array[i]);
   }
   free(r_array);
   return 0;
 }
 
-void generate_r_array(int *r_array, int num_elements, int seed) {
+void generate_r_array(int *r_array, int num_elements, int seed)
+{
   int i;
 
   srand(seed);
 
-  for (i = 0; i < num_elements; i++) {
+  for (i = 0; i < num_elements; i++)
+  {
     r_array[i] = rand();
   }
 }
 
-void *get_sum(int *array_pos, int length) {
+void *get_sum(int *array_pos, int length)
+{
   int i;
   int *sum = malloc(sizeof(int));
 
   *sum = 0;
 
-  for (i = 0; i < length; i++) {
+  for (i = 0; i < length; i++)
+  {
     *sum = (*sum + array_pos[i]) % 1000000;
   }
 
   return sum;
 }
 
-void *max(int *array, int length) {
+void *max(int *array, int length)
+{
   int *max = malloc(sizeof(int));
   int i;
   *max = array[0];
 
   /* Iterate through the array and update max if a larger element is found*/
-  for (i = 1; i < length; i++) {
-    if (array[i] > *max) {
+  for (i = 1; i < length; i++)
+  {
+    if (array[i] > *max)
+    {
       *max = array[i];
     }
   }
