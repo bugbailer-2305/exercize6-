66549cece0c6e2679b129270f6ed1281fb91a926
Naitik Gupta <naitikguptarocks@gmail.com>
2023-08-01 16:12:11 -0400

    completed implementation of array fragments

--- test.c
+++ test.c
@@ -8,26 +8,59 @@ struct ThreadArgs
   int *array_pos;
   int length;
 };
+typedef struct thread_struct
+{
+  int *array;
+  int length;
+  int sum;
+  int max;
+
+} Thread_struct;
 
 void generate_r_array(int *r_array, int num_elements, int seed);
-void *get_sum(int *array_pos, int length);
-void *max(int *array, int length);
+void *get_sum(Thread_struct *s);
+void *max(Thread_struct *s);
 
-void threads_function(function_type func, int max_threads)
+void threads_function(function_type func, int max_threads, Thread_struct *s)
 {
   pthread_t tids[max_threads + 1];
-  int i, ids[max_threads + 1];
+  int i, j, ids[max_threads + 1];
+  Thread_struct struc_array[max_threads + 1];
+  int part_array = s->length / max_threads;
+  int remainder_to_add = max_threads - (s->length % max_threads);
+  int displacement_variable = 0;
+  for (i = 1; i <= max_threads; i++)
+  {
+    Thread_struct *x = malloc(sizeof(Thread_struct));
+    x->array = malloc(part_array * sizeof(int));
+    x->length = part_array;
+    for (j = 0; j < part_array; j++)
+    {
+      if ((j + displacement_variable) < s->length)
+      {
+        x->array[j] = s->array[j + displacement_variable];
+      }
+      else
+      {
+        x->array[j] = 0;
+      }
+    }
+    struc_array[i] = *x;
+    free(x->array);
+    free(x);
+  }
 
   /* Creating threads */
   for (i = 1; i <= max_threads; i++)
   {
-    ids[i] = i;
-    pthread_create(&tids[i], NULL, func, &ids[i]);
+
+    pthread_create(&tids[i], NULL, func, &struc_array[i]);
     printf("Thread 0 created thread %d\n", i);
+    displacement_variable += part_array;
   }
 
   /* Waiting for threads to finish */
-  for (i = 1; i <= THREAD_CT; i++)
+  for (i = 1; i <= max_threads; i++)
   {
     pthread_join(tids[i], NULL);
     printf("Thread 0 reaped thread %d\n", i);
@@ -79,34 +112,34 @@ void create_part_array(int *r_array, int num_elements, int threads)
   int no_of_frags = num_elements % threads;
 }
 
-void *get_sum(int *array_pos, int length)
+void *get_sum(Thread_struct *s)
 {
   int i;
   int *sum = malloc(sizeof(int));
 
   *sum = 0;
 
-  for (i = 0; i < length; i++)
+  for (i = 0; i < s->length; i++)
   {
-    *sum = (*sum + array_pos[i]) % 1000000;
+    *sum = (*sum + s->array[i]) % 1000000;
   }
 
-  return sum;
+  s->sum = sum;
 }
 
-void *max(int *array, int length)
+void *max(Thread_struct *s)
 {
   int *max = malloc(sizeof(int));
   int i;
-  *max = array[0];
+  *max = s->array[0];
 
   /* Iterate through the array and update max if a larger element is found*/
-  for (i = 1; i < length; i++)
+  for (i = 1; i < s->length; i++)
   {
-    if (array[i] > *max)
+    if (s->array[i] > *max)
     {
-      *max = array[i];
+      *max = s->array[i];
     }
   }
-  return max;
+  s->max = max;
 }
